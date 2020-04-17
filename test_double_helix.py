580cf9a22a268c8ae20fd10cc1904fa439d6a320
Khaveesh Nagappan <khaveesh@gmail.com>
2020-04-17 17:19:05 +0530

    Refactored to read testcases from file

--- test_double_helix.py
+++ test_double_helix.py
@@ -11,23 +11,17 @@ class Test(unittest.TestCase):
 
     def setUp(self) -> None:
         """Testcase class is setup to run tests."""
-        in_lists = (
-            (
-                "13 3 5 7 9 20 25 30 40 55 56 57 60 62",
-                "11 1 4 7 11 14 25 44 47 55 57 100",
-                450,
-            ),
-            ("4 -5 100 1000 1005", "3 -12 1000 1001", 2100),
-        )
-
-        self.lists = (
-            (
-                tuple(map(int, iter_list[0].split())),
-                tuple(map(int, iter_list[1].split())),
-                iter_list[2],
+        with open("testcases.txt") as file_desc:
+            inp = tuple(filter(None, file_desc.read().splitlines()))
+            temp_list = (inp[index : index + 3] for index in range(0, len(inp), 3))
+            self.lists = (
+                (
+                    tuple(map(int, iter_list[0].split())),
+                    tuple(map(int, iter_list[1].split())),
+                    int(iter_list[2]),
+                )
+                for iter_list in temp_list
             )
-            for iter_list in in_lists
-        )
 
     def test_double_helix_dp(self) -> None:
         """Function to test the dynamic programming solution using unittest framework."""
