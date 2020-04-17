ec59fdd8af2663d8cd0126835cb2e0b57504cc09
Khaveesh Nagappan <khaveesh@gmail.com>
2020-04-17 14:34:59 +0530

    Refactored to use unittest instead of pytest

--- test_double_helix.py
+++ test_double_helix.py
@@ -1,27 +1,50 @@
 # coding=utf-8
 """Tests the solution."""
-import anarc05b_double_helix as db
+import unittest
+
+import anarc05b_double_helix as dp
+import anarc05b_double_helix_greedy as grdy
 
 
-def test_double_helix() -> None:
-    """Function to test the double helix solution using py.test."""
-    in_lists = (
-        (
-            "13 3 5 7 9 20 25 30 40 55 56 57 60 62",
-            "11 1 4 7 11 14 25 44 47 55 57 100",
-            450,
-        ),
-        ("4 -5 100 1000 1005", "3 -12 1000 1001", 2100),
-    )
+class Test(unittest.TestCase):
+    """Unittest requires a class of TestCase to run."""
 
-    lists = (
-        (
-            tuple(map(int, iter_list[0].split())),
-            tuple(map(int, iter_list[1].split())),
-            iter_list[2],
+    def setUp(self) -> None:
+        """Testcase class is setup to run tests."""
+        in_lists = (
+            (
+                "13 3 5 7 9 20 25 30 40 55 56 57 60 62",
+                "11 1 4 7 11 14 25 44 47 55 57 100",
+                450,
+            ),
+            ("4 -5 100 1000 1005", "3 -12 1000 1001", 2100),
         )
-        for iter_list in in_lists
-    )
 
-    for element in lists:
-        assert db.DoubleHelix(element[0], element[1]).solve() == element[2]
+        self.lists = (
+            (
+                tuple(map(int, iter_list[0].split())),
+                tuple(map(int, iter_list[1].split())),
+                iter_list[2],
+            )
+            for iter_list in in_lists
+        )
+
+    def test_double_helix_dp(self) -> None:
+        """Function to test the dynamic programming solution using unittest framework."""
+        for element in self.lists:
+            with self.subTest(element):
+                self.assertEqual(
+                    dp.DoubleHelix(element[0], element[1]).solve(), element[2],
+                )
+
+    def test_double_helix_greedy(self) -> None:
+        """Function to test the greedy solution using unittest framework."""
+        for element in self.lists:
+            with self.subTest(element):
+                self.assertEqual(
+                    grdy.DHG(element[0], element[1]).solve(), element[2],
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
