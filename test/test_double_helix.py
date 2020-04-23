43bde04ed668e31ae631b656f8bf3d8ecce54985
DaKeiser <rithwiksaim@gmail.com>
2020-04-24 00:20:01 +0530

    Added tests in sep folder

---
+++ test/test_double_helix.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+"""Tests the solution."""
+import unittest
+
+from code import anarc05b_double_helix as dp
+from code import anarc05b_double_helix_greedy as grdy
+
+
+class Test(unittest.TestCase):
+    """Unittest requires a class of TestCase to run."""
+
+    def setUp(self) -> None:
+        """Testcase class is setup to run tests."""
+        with open("correct_tests.txt") as file_desc:
+            # Read all the lines from file, removing empty lines and \n's
+            raw_inp = filter(None, file_desc.read().splitlines())
+            # Split string of integers into list of ints
+            inp = [list(map(int, string.split())) for string in raw_inp]
+            # Encapsulate the first, second and soln for each testcase in a separate tuple
+            self.testcases = [
+                (iter_list[0], iter_list[1], iter_list[2][0])
+                for iter_list in zip(inp[::3], inp[1::3], inp[2::3])
+            ]
+
+    def test_double_helix_dp(self) -> None:
+        """Function to test the dynamic programming solution using unittest framework."""
+        for testcase in self.testcases:
+            with self.subTest(testcase):
+                self.assertEqual(
+                    dp.DoubleHelix(testcase[0], testcase[1]).solve(), testcase[2],
+                )
+
+    def test_double_helix_greedy(self) -> None:
+        """Function to test the greedy solution using unittest framework."""
+        for testcase in self.testcases:
+            with self.subTest(testcase):
+                self.assertEqual(
+                    grdy.DHG(testcase[0], testcase[1]).solve(), testcase[2],
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
